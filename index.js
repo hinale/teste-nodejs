79538d728c0b9d0277c99fe0fd001addcfde8beb
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-07-16 12:03:27 -0300

    primeira github action

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/hello', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('world');
+  res.end('WORLD');
 });
 
 app.use('/', router);
