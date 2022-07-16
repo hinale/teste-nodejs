7ba11617d14a017c59c2f046b8b2af1fbe416db8
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-07-16 12:35:24 -0300

    removendo coisas desnecessários do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,21 +5,5 @@ module.exports = {
     watch: '.',
     watch_delay: 1000,
     ignore_watch: ["node_modules"],
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
   }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
 };
--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const router = express.Router();
 router.get('/hello', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('WORLD');
+  res.end('world');
 });
 
-app.use('/', router);
+app.use('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('v.1.0.0');
+});
 app.listen(process.env.PORT || 3000);
