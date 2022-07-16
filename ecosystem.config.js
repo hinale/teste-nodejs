b34bfcd5f55a07e55b52db1e4b9ec94d6c74403c
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-07-16 12:29:28 -0300

    testes

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'index.js',
+    watch: '.',
+    watch_delay: 1000,
+    ignore_watch: ["node_modules"],
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
